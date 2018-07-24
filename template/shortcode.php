e7ddf96151584c4de576350f4547ce6736f4626b
Adrià Garrido <agarri53@xtec.cat>
2018-07-24 16:38:55 +0200

    Error - Tab responsive #614

--- template/shortcode.php
+++ template/shortcode.php
@@ -1,18 +1,19 @@
 <?php
 add_shortcode( 'TABS_R', 'TABS_R_ShortCode' );
 function TABS_R_ShortCode( $Id ) {
-	ob_start();	
-	if(!isset($Id['id'])) 
-	 {
+	ob_start();
+	if(!isset($Id['id']))
+	{
 		$WPSM_Tabs_ID = "";
-	 } 
-	else 
+	}
+	else
 	{
 		$WPSM_Tabs_ID = $Id['id'];
 	}
-	require("content.php"); 
-	
-	wp_reset_query();
+	require("content.php");
+
+	// XTEC MODIFICAT - Change wp_reset_query to wp_reset_postdata to prevent errors with loop inside loop. - 2018.07.24 @adriagarrido
+	wp_reset_postdata();
     return ob_get_clean();
 }
 ?>
