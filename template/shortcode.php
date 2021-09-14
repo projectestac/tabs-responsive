b222452e117807c123a1356925e46495c1efa3a4
Toni Ginard <toni.ginard@gmail.com>
2021-09-14 13:29:05 +0200

    Removed call to wp_reset_query() in front page because it breaks the loading of posts

--- template/shortcode.php
+++ template/shortcode.php
@@ -14,7 +14,20 @@ function TABS_R_ShortCode( $Id ) {
 	require("content.php"); 
 	
 	
+// XTEC ************ AFEGIT - Resetting the query breaks the loading of posts in main page
+// 2021.09.14 @aginard
+// 2022.04.11 @aginard
+    if (is_page()) {
+// ************ FI
+
 	wp_reset_query();
+
+// XTEC ************ AFEGIT - Resetting the query breaks the loading of posts in main page
+// 2021.09.14 @aginard
+// 2022.04.11 @aginard
+    }
+// ************ FI
+
     return ob_get_clean();
 }
 ?>
